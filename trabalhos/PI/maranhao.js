a9201ed2acce5c2ca54261c3140db863147693f5
Kevin-luan <cardosoluan3006@gmail.com>
2026-05-30 02:09:00 -0300

    PI maranhao Tela Praia

--- trabalhos/PI/maranhao.js
+++ trabalhos/PI/maranhao.js
@@ -1,18 +1,20 @@
 let carro;
+let coqueiro;
 let x=10;
 let condicao = false;
+let decerCarro = false;
+let nadar;
 function preload() {
 
     
   carro= loadImage('GIF/carro.gif');
+  coqueiro = loadImage('img/imgParaCanva/coqueiro.png');
+  nadar = loadImage('GIF/nadar.gif');
 }
 
 
 function setup() {
   createCanvas(800, 600);
-   
-   
-
 
 }
 function mouseClicked() {
@@ -21,7 +23,18 @@ if (mouseX < 250) {
   }else if (mouseX > 250) {
  condicao = false;
   }
-    
+  if(mouseX >650 && mouseX <750 && mouseY > 550 && mouseY <570){
+
+    decerCarro = true;
+  }
+   if(mouseX >100 && mouseX <150 && mouseY > 550 && mouseY <570){
+
+    decerCarro = false;
+  }
+
+
+
+   
 }
 function draw() {
 
@@ -29,6 +42,9 @@ function draw() {
    background(135, 206, 235);
  fill(255, 223, 0);
  circle(700, 100, 100);
+
+
+ // ir para praia
 
 
  // mar
@@ -81,5 +97,35 @@ else{
  image(carro, 500, 400, 100, 100);
     
 }
+ fill(255, 223, 0);
+ rect(650, 550, 80, 20);
+ textSize(12);
+ fill(255);
+ text("Ir para praia", 660, 565);
+ fill(255, 223, 0);
+  rect(100, 550, 50, 20);
 
+      //======================= ir para praia    ============================= 
+ if(decerCarro == true){
+  background(135, 206, 235);
+  fill(255, 223, 0);
+ circle(700, 100, 100);
+   // mar
+ stroke(255);  
+  fill(0, 119, 190);
+ rect(0, 250, width, 300)
+
+// areia 
+
+   fill(245, 222, 179);
+ rect(0, 400, 800, 500);
+ image(coqueiro, 50, 380, 200, 200);
+  image(coqueiro, 580, 380, 200, 200);
+  fill(255, 223, 0);
+  rect(100, 550, 50, 20);
+  textSize(12);
+ fill(255);
+ text("Voltar", 110, 565);
+  image(nadar, 500, 250, 100, 100);
+ }
 }
