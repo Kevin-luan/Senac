14b9c7915aea969d8cbb60a6590335414d975d13
Kevin-luan <cardosoluan3006@gmail.com>
2026-09-17 14:35:17 -0300

    Lista03 exe06

---
+++ Algoritmos e Programacao/Segundo semestre/Lista03/exe06/exe06/exe06.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//Escreva a função para cálculo do N - ésimo termo da sequência de Fibonacci
+//utilizando recursividade.
+
+int fibonacci(int i) {
+
+	if (i == 0) {
+		return 0;
+	}
+	if (i == 1) { return 1; }
+	
+		return fibonacci(i - 1) + fibonacci(i - 2);
+	
+
+
+
+}
+
+
+int main() {
+	int p = 9;
+
+		printf("%d", fibonacci(p));
+		return 0;
+
+	
+
+	
+}
